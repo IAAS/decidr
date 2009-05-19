8abd1feafcd23ad8b3cad85fa4f3212f1655a6d4
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-05-19 11:52:35 +0000

    RR: changed folder structure because of maven

---
+++ src/HumanTaskWS/src/main/java/de/decidr/webservices/humantask/BPELCallbackClient.java
@@ -0,0 +1,88 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.decidr.webservices.humantask;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import javax.xml.ws.WebEndpoint;
+import javax.xml.ws.WebServiceClient;
+import javax.xml.ws.WebServiceFeature;
+
+/**
+ * A web service client for the callback method provided by the BPEL process.
+ * 
+ * @author RR
+ */
+// FIXME: complete as soon as known
+@WebServiceClient(name = "BPELCallbackClient", targetNamespace = "http://decidr.de/TODO", wsdlLocation = "TODO")
+public class BPELCallbackClient extends Service {
+
+    /**
+     * TODO: add comment
+     * 
+     * @param wsdlDocumentLocation
+     * @param serviceName
+     */
+    protected BPELCallbackClient(URL wsdlDocumentLocation, QName serviceName) {
+        super(wsdlDocumentLocation, serviceName);
+    }
+
+    /**
+     * TODO: add comment
+     * 
+     * @throws MalformedURLException
+     */
+    public BPELCallbackClient() throws MalformedURLException {
+        // FIXME: get WSDL location from config
+        // FIXME: add namespace
+        // FIXME: add correct web service name
+        this(new URL("TODO"), new QName("http://decidr.de/TODO",
+                "BPELCallbackInterface"));
+    }
+
+    /**
+     * TODO: add comment
+     * 
+     * @return
+     */
+    // FIXME: add namespace
+    // FIXME: add correct web service name
+    @WebEndpoint(name = "BPELCallbackInterfaceHttpSoap12Endpoint")
+    public BPELCallbackInterface getBPELCallbackInterfacePort() {
+        return super.getPort(new QName("http://decidr.de/TODO",
+                "BPELCallbackInterfaceHttpSoap12Endpoint"),
+                BPELCallbackInterface.class);
+    }
+
+    /**
+     * TODO: add comment
+     * 
+     * @param features
+     * @return
+     */
+    // FIXME: add namespace
+    // FIXME: add correct web service name
+    @WebEndpoint(name = "BPELCallbackInterfaceHttpSoap12Endpoint")
+    public BPELCallbackInterface getBPELCallbackInterfacePort(
+            WebServiceFeature... features) {
+        return super.getPort(new QName("http://decidr.de/TODO",
+                "BPELCallbackInterfaceHttpSoap12Endpoint"),
+                BPELCallbackInterface.class, features);
+    }
+}
---
+++ src/HumanTaskWS/src/main/java/de/decidr/webservices/humantask/BPELCallbackInterface.java
@@ -0,0 +1,30 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package de.decidr.webservices.humantask;
+
+import javax.jws.WebService;
+
+/**
+ * This is a temporary interface for the BEPL callback method web service
+ * interface. It will be replaced by whatever the BEPL libraries offer as soon
+ * as they do.
+ * 
+ * @author RR
+ */
+@WebService()
+public interface BPELCallbackInterface {
+
+}
