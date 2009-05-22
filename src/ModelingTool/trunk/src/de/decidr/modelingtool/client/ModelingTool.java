6ccc162a095075e7069d70eb688592ebff058250
schlaajs <schlaajs@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-05-22 14:34:56 +0000

    JS: Imported gwt project data

---
+++ src/ModelingTool/trunk/src/de/decidr/modelingtool/client/ModelingTool.java
@@ -0,0 +1,16 @@
+package de.decidr.modelingtool.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class ModelingTool implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+	}
+}
