92569d017e83c500affdcc9c7fbaddd68177aeb8
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-01 16:48:32 +0000

    DH: added logger stubs.

--- src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
+++ src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
@@ -3,17 +3,16 @@ package de.decidr.model.logging;
 import org.apache.log4j.Logger;
 
 /**
- * FIXME add maven dependencies for log4j to libs project
- *
+ * TODO replace stubs with DecidR centralized logging. Add documentation.
  */
 public class DefaultLogger {
 
-	public static Logger getLogger(String name) {
-		throw new UnsupportedOperationException();
-	}
+    public static Logger getLogger(String name) {
+        return Logger.getLogger(name);
+    }
 
-	@SuppressWarnings("unchecked")
-	public static Logger getLogger(Class name) {
-		throw new UnsupportedOperationException();
-	}
+    @SuppressWarnings("unchecked")
+    public static Logger getLogger(Class clazz) {
+        return Logger.getLogger(clazz);
+    }
 }
