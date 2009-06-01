8781b60769e7690770e6d6bb76edf6d48bb43b7f
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-01 18:15:20 +0000

    DH: revised logger stub, still needs proper usage of the JDCB appender.

--- src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
+++ src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
@@ -1,18 +1,37 @@
 package de.decidr.model.logging;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 
 /**
  * TODO replace stubs with DecidR centralized logging. Add documentation.
  */
 public class DefaultLogger {
 
-    public static Logger getLogger(String name) {
-        return Logger.getLogger(name);
+    private static class Factory implements LoggerFactory {
+
+        @Override
+        public Logger makeNewLoggerInstance(String name) {
+            Logger result = LogManager.exists(name);
+            if (result == null) {
+                result = LogManager.getLogger(name);
+                result.addAppender(new ConsoleAppender());
+            }
+
+            return result;
+        }
     }
 
-    @SuppressWarnings("unchecked")
+    private static Factory factory = new Factory();
+
     public static Logger getLogger(Class clazz) {
-        return Logger.getLogger(clazz);
+        return LogManager.getLogger(clazz.getCanonicalName(), factory);
     }
+
+    public static Logger getLogger(String name) {
+        return LogManager.getLogger(name, factory);
+    }
+
 }
