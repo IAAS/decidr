080b0b27254e1f7c0b345b0779c84b12c098bdc6
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-13 15:24:37 +0000

    DH: implemented UserCommand.

--- src/model/trunk/src/main/java/de/decidr/model/commands/user/UserCommand.java
+++ src/model/trunk/src/main/java/de/decidr/model/commands/user/UserCommand.java
@@ -1,15 +1,51 @@
 package de.decidr.model.commands.user;
 
-import java.util.Collection;
+import org.hibernate.Session;
 
 import de.decidr.model.commands.AclEnabledCommand;
+import de.decidr.model.entities.User;
+import de.decidr.model.exceptions.EntityNotFoundException;
 import de.decidr.model.permissions.Permission;
 import de.decidr.model.permissions.Role;
 
-public abstract class UserCommand extends AclEnabledCommand{
+/**
+ * Abstract base class for commands that deal with users.
+ * 
+ * @author Markus Fischer
+ * @author Daniel Huss
+ * @version 0.1
+ */
+public abstract class UserCommand extends AclEnabledCommand {
 
-    public UserCommand(Role role, Collection<Permission> permission) {
-        super(role, permission);
-        // TODO Auto-generated constructor stub
+    private Long userId;
+
+    /**
+     * Creates a new UserCommand.
+     * 
+     * @param role
+     * @param additionalPermissions
+     */
+    public UserCommand(Role role, Long userId) {
+        super(role, (Permission) null);
+        this.userId = userId;
+    }
+
+    /**
+     * Fetches the user from the database
+     * 
+     * @param session
+     *            the Hibernate session to use for lookup.
+     * @return
+     * @throws EntityNotFoundException
+     *             if no user is found
+     */
+    protected User fetchUser(Session session) throws EntityNotFoundException {
+        User user = (User) session.get(User.class, userId);
+
+        if (user == null) {
+            throw new EntityNotFoundException(User.class, userId);
+        }
+
+        return user;
     }
 }
