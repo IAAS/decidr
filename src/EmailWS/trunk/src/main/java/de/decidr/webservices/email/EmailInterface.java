75917b4ee3bbe0499df46d27d6311c71dcb6d3e1
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-13 07:22:31 +0000

    RR: moved exceptions to model and added file comments to generated files

--- src/EmailWS/trunk/src/main/java/de/decidr/webservices/email/EmailInterface.java
+++ src/EmailWS/trunk/src/main/java/de/decidr/webservices/email/EmailInterface.java
@@ -1,12 +1,29 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package de.decidr.webservices.email;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.ws.Action;
 
 import de.decidr.model.exceptions.TransactionException;
+import de.decidr.model.soap.exceptions.IoExceptionWrapper;
+import de.decidr.model.soap.exceptions.MalformedURLExceptionWrapper;
+import de.decidr.model.soap.exceptions.MessagingExceptionWrapper;
 import de.decidr.model.soap.types.AbstractUserList;
 import de.decidr.model.soap.types.IDList;
 import de.decidr.model.soap.types.StringMap;
@@ -18,7 +35,6 @@ import de.decidr.model.soap.types.StringMap;
  */
 
 @WebService(targetNamespace = "http://decidr.de/webservices/Email", name = "EmailPT")
-@XmlSeeAlso( { ObjectFactory.class })
 public interface EmailInterface {
 
     @Action(input = "urn:sendEmail", output = "urn:sendEmailResponse", fault = {})
