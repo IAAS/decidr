7ef0e4ea03e840db9af9e147873b6c3cf66e0592
geoffrex <geoffrex@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-16 12:34:58 +0000

    GH: added components and actions for password reset (if password lost)

---
+++ src/WebPortal/trunk/src/main/java/de/decidr/ui/controller/ResetPasswordAction.java
@@ -0,0 +1,63 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.ui.controller;
+
+/**
+ * TODO: add comment
+ *
+ * @author GH
+ */
+
+import javax.servlet.http.HttpSession;
+
+import com.vaadin.data.Item;
+import com.vaadin.service.ApplicationContext;
+import com.vaadin.terminal.gwt.server.WebApplicationContext;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+import de.decidr.ui.view.ChangeEmailComponent;
+import de.decidr.ui.view.Main;
+import de.decidr.ui.view.ResetPasswordComponent;
+import de.decidr.model.facades.UserFacade;
+import de.decidr.model.permissions.UserRole;
+
+public class ResetPasswordAction implements ClickListener{
+    
+    //TODO: remove // below, code is disabled for testing, since the model causes errors
+    
+    //private ApplicationContext ctx = Main.getCurrent().getContext();
+    //private WebApplicationContext webCtx = (WebApplicationContext)ctx;
+    //private HttpSession session = webCtx.getHttpSession();
+    
+    //private Long userId = (Long)session.getAttribute("userId");
+    //private UserFacade userFacade = new UserFacade(new UserRole(userId));
+    
+    private Item request = null;
+    
+    @Override
+    public void buttonClick(ClickEvent event) {
+        request = ResetPasswordComponent.getInstance().getRequestForm();
+        
+        //TODO: add validator to ResetPasswordComponent
+        
+        //userFacade.requestPasswordReset(request.getItemProperty("email").getValue().toString());
+        Main.getCurrent().getMainWindow().showNotification("new password sent to: " + request.getItemProperty("email").getValue().toString());
+        Main.getCurrent().getMainWindow().removeWindow(ResetPasswordComponent.getInstance());
+        
+    }
+}
---
+++ src/WebPortal/trunk/src/main/java/de/decidr/ui/view/ResetPasswordComponent.java
@@ -0,0 +1,112 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.ui.view;
+
+/**
+ * TODO: add comment
+ *
+ * @author GH
+ */
+
+import java.util.Arrays;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.validator.NullValidator;
+import com.vaadin.ui.FieldFactory;
+import com.vaadin.ui.Form;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Window;
+
+import de.decidr.ui.controller.ResetPasswordAction;
+import de.decidr.ui.controller.HideResetPasswordAction;
+
+public class ResetPasswordComponent extends Window {
+
+    private static ResetPasswordComponent resetPasswordComponent = null;
+    
+    
+    private VerticalLayout verticalLayout = null;
+    private HorizontalLayout horizontalLayout = null;
+    
+    private Form requestForm = null;
+    
+    private TextField email = null;
+    
+    private Button submitButton = null;
+    private Button cancelButton = null;
+    
+    public Item getRequestForm(){
+        return requestForm;
+    }
+    
+    private ResetPasswordComponent(){
+        init();
+    }
+    
+    private void init(){
+        requestForm = new Form();
+        requestForm.setWriteThrough(false);
+        
+        
+        verticalLayout = new VerticalLayout();
+        verticalLayout.setSpacing(true);
+        verticalLayout.setMargin(true);
+        verticalLayout.setSizeUndefined();
+        
+        horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setSpacing(true);
+        horizontalLayout.setMargin(false);
+        horizontalLayout.setSizeUndefined();
+        
+        
+        email = new TextField();
+        email.setCaption("User Name or E-Mail:");
+        email.setColumns(21);
+        
+        submitButton = new Button("Reset Password", new ResetPasswordAction());
+        cancelButton = new Button("Cancel", new HideResetPasswordAction());
+
+        requestForm.setWidth(400, Form.UNITS_PIXELS);
+        requestForm.addField("email", email);
+        
+        verticalLayout.addComponent(requestForm);
+        horizontalLayout.addComponent(submitButton);
+        horizontalLayout.addComponent(cancelButton);
+        verticalLayout.addComponent(horizontalLayout);
+        
+        horizontalLayout.setComponentAlignment(cancelButton, "right bottom");
+        horizontalLayout.setComponentAlignment(submitButton, "right bottom");
+
+        verticalLayout.setComponentAlignment(horizontalLayout, "right bottom");
+        
+        this.setModal(true);
+        this.setResizable(false);
+        this.setCaption("Reset Password");
+        this.setContent(verticalLayout);
+    }
+    
+    public static ResetPasswordComponent getInstance(){
+        if (resetPasswordComponent == null){
+            resetPasswordComponent = new ResetPasswordComponent();
+        }
+        return resetPasswordComponent;
+    }
+}
