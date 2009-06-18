0cc3474e93e4c6d09eaa13e6d73a727119fe32b6
geoffrex <geoffrex@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-18 13:42:45 +0000

    GH: Added InvitationDialogComponent & two new actions

---
+++ src/WebPortal/trunk/src/main/java/de/decidr/ui/view/InvitationDialogComponent.java
@@ -0,0 +1,110 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.ui.view;
+
+/**
+ * TODO: add comment
+ *
+ * @author GH
+ */
+
+import com.vaadin.data.Item;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Form;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+import de.decidr.ui.controller.ChangeEmailAction;
+import de.decidr.ui.controller.ConfirmInvitationAction;
+import de.decidr.ui.controller.HideChangeEmailAction;
+import de.decidr.ui.controller.RefuseInvitationAction;
+
+public class InvitationDialogComponent extends Window {
+
+    private static InvitationDialogComponent invitationDialogComponent = null;
+    
+    private Long invitationId = null;
+    
+    private VerticalLayout verticalLayout = null;
+    private HorizontalLayout horizontalLayout = null;
+    
+    private Label infoLabel = null;
+        
+    private Button submitButton = null;
+    private Button cancelButton = null;
+        
+    public InvitationDialogComponent(Long invId){
+        invitationId = invId;
+        init();
+    }
+    
+    private void init(){
+        
+        verticalLayout = new VerticalLayout();
+        verticalLayout.setSpacing(true);
+        verticalLayout.setMargin(true);
+        verticalLayout.setSizeUndefined();
+        
+        horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setSpacing(true);
+        horizontalLayout.setMargin(false);
+        horizontalLayout.setSizeUndefined();
+        
+        infoLabel = new Label("You have been invited to join a new tenant: TENANTNAME",
+                        Label.CONTENT_XHTML);
+        infoLabel.setWidth(350,Label.UNITS_PIXELS);
+        
+
+        
+        submitButton = new Button("Accept Invitation", new ConfirmInvitationAction(invitationId));
+        cancelButton = new Button("Refuse Invitation", new RefuseInvitationAction(invitationId));
+
+
+        verticalLayout.addComponent(infoLabel);
+        horizontalLayout.addComponent(submitButton);
+        horizontalLayout.addComponent(cancelButton);
+        verticalLayout.addComponent(horizontalLayout);
+        
+        horizontalLayout.setComponentAlignment(cancelButton, "right bottom");
+        horizontalLayout.setComponentAlignment(submitButton, "right bottom");
+
+        verticalLayout.setComponentAlignment(horizontalLayout, "right bottom");
+        
+        this.setModal(true);
+        this.setName("InvitationDialog");
+        this.setResizable(false);
+        this.setCaption("Change E-mail Address");
+        this.setContent(verticalLayout);
+    }
+    
+    public static InvitationDialogComponent getInstance(){
+        //if (invitationDialogComponent == null){
+        //    invitationDialogComponent = new InvitationDialogComponent(invId);
+        //}
+        return invitationDialogComponent;
+    }
+    
+    public static InvitationDialogComponent getInstance(Long invId){
+        //if (invitationDialogComponent == null){
+            invitationDialogComponent = new InvitationDialogComponent(invId);
+        //}
+        return invitationDialogComponent;
+    }
+}
