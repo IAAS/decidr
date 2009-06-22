2472f3c1189917fe97d0add02c75deccbe643852
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-22 13:52:46 +0000

    DH: added DecidrGlobals containing some "tool methods".

---
+++ src/model/trunk/src/main/java/de/decidr/model/DecidrGlobals.java
@@ -0,0 +1,53 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.model;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.hibernate.Session;
+
+import de.decidr.model.entities.SystemSettings;
+
+/**
+ * @author Daniel Huss
+ * @version 0.1
+ */
+public class DecidrGlobals {
+
+    /**
+     * For scalability, the entire application should use the same time zone
+     * (UTC).
+     * 
+     * @return the current system time (UTC)
+     */
+    public static Calendar getTime() {
+        return Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    }
+
+    /**
+     * Fetches the current system settings.
+     * 
+     * @param session
+     * @return the current system settings.
+     */
+    public static SystemSettings getSettings(Session session) {
+        String hql = "from SystemSettings limit 1";
+        return (SystemSettings) session.createQuery(hql).uniqueResult();
+    }
+
+}
