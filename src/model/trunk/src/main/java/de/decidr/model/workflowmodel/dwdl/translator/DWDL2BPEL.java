eee7a872461b589d1af3a7541e24ffc1c8ecc10d
modood.alvi <modood.alvi@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-06-22 20:53:43 +0000

    MA: BPEL imports finished

--- src/model/trunk/src/main/java/de/decidr/model/workflowmodel/dwdl/translator/DWDL2BPEL.java
+++ src/model/trunk/src/main/java/de/decidr/model/workflowmodel/dwdl/translator/DWDL2BPEL.java
@@ -18,9 +18,17 @@ package de.decidr.model.workflowmodel.dwdl.translator;
 
 import java.util.List;
 
-import de.decidr.model.workflowmodel.bpel.ObjectFactory;
-import de.decidr.model.workflowmodel.bpel.TFaultHandlers;
+import javax.xml.namespace.QName;
 
+import de.decidr.model.workflowmodel.bpel.ObjectFactory;
+import de.decidr.model.workflowmodel.bpel.TDocumentation;
+import de.decidr.model.workflowmodel.bpel.TImport;
+import de.decidr.model.workflowmodel.bpel.TProcess;
+import de.decidr.model.workflowmodel.dwdl.TDescription;
+import de.decidr.model.workflowmodel.dwdl.TFaultHandler;
+import de.decidr.model.workflowmodel.dwdl.TNodes;
+import de.decidr.model.workflowmodel.dwdl.TRoles;
+import de.decidr.model.workflowmodel.dwdl.TWorkflow;
 
 /**
  * This class converts a given DWDL object and returns the resulting BPEL.
@@ -34,19 +42,71 @@ public class DWDL2BPEL {
     private final String QUERYLANGUAGE = "urn:oasis:names:tc:wsbpel:2.0:sublang:xpath1.0";
     private final String SUPPRESSJOINFAILURE = "no";
     private final String EXITONSTANDARDFAULT = "no";
+    private final String HTWSNAMESPACE = "http://decidr.de/webservices/HumanTask";
+    private final String EWSNAMESPACE = "http://decidr.de/webservices/Email";
+    private final String WSDLIMPORTTYPE = "http://schemas.xmlsoap.org/wsdl/";
+    private final String XSDIMPORTTYPE = "http://www.w3.org/2001/XMLSchema";
+    private final String DECIDRTYPESNS = "http://decidr.de/schema/DecidrTypes";
 
-    private de.decidr.model.workflowmodel.bpel.TProcess process = null;
-    private de.decidr.model.workflowmodel.dwdl.TWorkflow dwdl = null;
+    private TProcess process = null;
+    private TWorkflow dwdl = null;
     private ObjectFactory factory;
 
-    public de.decidr.model.workflowmodel.bpel.TProcess getBPEL(
-            de.decidr.model.workflowmodel.dwdl.TWorkflow dwdl) {
+    public TProcess getBPEL(TWorkflow dwdl) {
 
         this.dwdl = dwdl;
 
         factory = new ObjectFactory();
         process = factory.createTProcess();
-        
+        setDocumentation(dwdl.getDescription());
+        setProcessAttributes();
+        setImports();
+        setPartnerLinks();
+        setVariables(dwdl.getVariables().getVariable());
+        setVariablesFromRoles(dwdl.getRoles());
+        setCorrelationSets();
+        setFaultHandler(dwdl.getFaultHandler());
+        setActivity(dwdl.getNodes());
+        return process;
+    }
+
+    private void setActivity(TNodes nodes) {
+
+    }
+
+    private void setFaultHandler(TFaultHandler faultHandler) {
+
+    }
+
+    private void setCorrelationSets() {
+
+    }
+
+    private void setVariablesFromRoles(TRoles roles) {
+
+    }
+
+    private void setPartnerLinks() {
+
+    }
+
+    private void setImports() {
+        TImport htwsImport = factory.createTImport();
+        TImport ewsImport = factory.createTImport();
+        TImport dtImport = factory.createTImport();
+        htwsImport.setNamespace(HTWSNAMESPACE);
+        htwsImport.setImportType(WSDLIMPORTTYPE);
+        htwsImport.setLocation("HumanTask.wsdl");
+        ewsImport.setNamespace(EWSNAMESPACE);
+        ewsImport.setImportType(WSDLIMPORTTYPE);
+        ewsImport.setLocation("Email.wsdl");
+        dtImport.setNamespace(DECIDRTYPESNS);
+        dtImport.setImportType(XSDIMPORTTYPE);
+        dtImport.setLocation("DecidrTypes.xsd");
+    }
+
+    private void setProcessAttributes() {
+        process.setName(dwdl.getName() + "_" + dwdl.getId());
         process.setTargetNamespace(dwdl.getTargetNamespace());
         process.setExpressionLanguage(EXPRESSIONLANGUAGE);
         process.setQueryLanguage(QUERYLANGUAGE);
@@ -56,33 +116,24 @@ public class DWDL2BPEL {
         process
                 .setExitOnStandardFault(de.decidr.model.workflowmodel.bpel.TBoolean
                         .fromValue(EXITONSTANDARDFAULT));
-        process.getImport().add(createImports());
-
-        process.setPartnerLinks(createPartnerLinks());
-        process.getDocumentation().add(getDocumentation(dwdl.getDescription()));
-        process.setVariables(getVariables(dwdl.getVariables()));
-        process.getVariables().getVariable().addAll(
-                createVariablesFromRoles(dwdl.getRoles()));
-        process.setCorrelationSets(createCorrelationSets());
-        process.setFaultHandlers(createFaultHandlers()); 
-        return null;
     }
 
-    private de.decidr.model.workflowmodel.bpel.TImport createImports() {
-
-        return null;
-    }
-    
-
-    private de.decidr.model.workflowmodel.bpel.TDocumentation getDocumentation(
-            de.decidr.model.workflowmodel.dwdl.TDescription description) {
-        de.decidr.model.workflowmodel.bpel.TDocumentation documentation = factory.createTDocumentation();
+    private void setDocumentation(TDescription description) {
+        TDocumentation documentation = factory.createTDocumentation();
         documentation.getContent().addAll(description.getContent());
-        return null;
+
     }
 
-    private de.decidr.model.workflowmodel.bpel.TVariables getVariables(
-            de.decidr.model.workflowmodel.dwdl.TVariables variables) {
+    private void setVariables(
+            List<de.decidr.model.workflowmodel.dwdl.TVariable> variables) {
+        for (de.decidr.model.workflowmodel.dwdl.TVariable v : variables) {
+            de.decidr.model.workflowmodel.bpel.TVariable var = factory
+                    .createTVariable();
+            var.setName(v.getName());
+            var.setType(new QName("xsd:" + v.getType()));
+            process.getVariables().getVariable().add(var);
+        }
+
         return null;
     }
 
@@ -134,15 +185,22 @@ public class DWDL2BPEL {
     private de.decidr.model.workflowmodel.bpel.TPartnerLinks createPartnerLinks() {
         return null;
     }
-    
+
     private de.decidr.model.workflowmodel.bpel.TCorrelationSets createCorrelationSets() {
-        
+
         return null;
     }
-    
-    private TFaultHandlers createFaultHandlers() {
-        
+
+    private de.decidr.model.workflowmodel.bpel.TFaultHandlers createFaultHandlers(
+            de.decidr.model.workflowmodel.dwdl.TFaultHandler faultHandler) {
+
         return null;
     }
-    
+
+    private de.decidr.model.workflowmodel.bpel.TFlow getNodes(
+            de.decidr.model.workflowmodel.dwdl.TNodes nodes) {
+
+        return null;
+    }
+
 }
