ba87be0f62a6d2cac6307c6bc26d6529cbb05fed
modood.alvi <modood.alvi@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-07-02 13:39:25 +0000

    MA: Deployment Test component added

---
+++ src/model/trunk/src/main/java/de/decidr/model/workflowmodel/deployment/test/DeploymentTest.java
@@ -0,0 +1,50 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.model.workflowmodel.deployment.test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import de.decidr.model.workflowmodel.dwdl.TWorkflow;
+
+/**
+ * TODO: add comment
+ *
+ * @author Modood Alvi
+ */
+public class DeploymentTest {
+
+    /**
+     * TODO: add comment
+     *
+     * @param args
+     * @throws JAXBException 
+     * @throws FileNotFoundException 
+     */
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        JAXBContext dwdlCntxt = JAXBContext.newInstance(TWorkflow.class);
+        Unmarshaller unmarshaller = dwdlCntxt.createUnmarshaller();
+        TWorkflow dwdlWorkflow = (TWorkflow) unmarshaller.unmarshal(new FileInputStream("sampleProcess"));
+        dwdlWorkflow.toString();
+
+    }
+
+}
