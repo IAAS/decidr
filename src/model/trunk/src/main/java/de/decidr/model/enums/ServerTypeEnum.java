c8eff4d82b9ea80a9f74b41f3b3ecd33e344f6dd
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-07-10 15:24:27 +0000

    DH: added enum of supported server types.

---
+++ src/model/trunk/src/main/java/de/decidr/model/enums/ServerTypeEnum.java
@@ -0,0 +1,25 @@
+package de.decidr.model.enums;
+
+/**
+ * Enumeration of supported server types.
+ * 
+ * @author Thomas Karsten
+ * @author Daniel Huss
+ * @version 0.1
+ */
+public enum ServerTypeEnum {
+
+    /**
+     * Apache ODE
+     */
+    Ode,
+    /**
+     * Apache Synapse ESB
+     */
+    Esb,
+    /**
+     * Web front-end / gui
+     */
+    WebPortal
+
+}
