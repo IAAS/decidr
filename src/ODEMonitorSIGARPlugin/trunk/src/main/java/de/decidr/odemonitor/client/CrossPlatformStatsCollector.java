a4ee66fc7aecd0e07ed4cecda362759f2f4b625f
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-08-03 13:32:47 +0000

    RR: added maven project for ODE Monitor SIGAR API plugin

---
+++ src/ODEMonitorSIGARPlugin/trunk/src/main/java/de/decidr/odemonitor/client/CrossPlatformStatsCollector.java
@@ -0,0 +1,44 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.odemonitor.client;
+
+/**
+ * RR: add comment
+ *
+ * @author Reinhold
+ */
+public class CrossPlatformStatsCollector extends AbstractOSStatsCollector {
+
+    /* (non-Javadoc)
+     * @see de.decidr.odemonitor.client.AbstractOSStatsCollector#getCPULoad()
+     */
+    @Override
+    public int getCPULoad() {
+        // RR Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see de.decidr.odemonitor.client.AbstractOSStatsCollector#getMemLoad()
+     */
+    @Override
+    public int getMemLoad() {
+        // RR Auto-generated method stub
+        return 0;
+    }
+
+}
