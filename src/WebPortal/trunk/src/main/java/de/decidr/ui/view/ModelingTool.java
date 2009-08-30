b8104df424ef9d53c64640c06e378ed81761d05d
aleks.tolev <aleks.tolev@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-08-30 09:06:04 +0000

    AT: Modified Modeling Tool. Implemented interface for communication with the Modeling Tool

--- src/WebPortal/trunk/src/main/java/de/decidr/ui/view/ModelingTool.java
+++ src/WebPortal/trunk/src/main/java/de/decidr/ui/view/ModelingTool.java
@@ -16,70 +16,141 @@
 
 package de.decidr.ui.view;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import com.vaadin.data.Item;
 import com.vaadin.terminal.PaintException;
 import com.vaadin.terminal.PaintTarget;
 import com.vaadin.ui.AbstractComponent;
+
+
+import de.decidr.model.acl.roles.UserRole;
+import de.decidr.model.exceptions.TransactionException;
+import de.decidr.model.facades.TenantFacade;
+import de.decidr.modelingtool.client.io.DataExchanger;
+import de.decidr.ui.data.Server;
 
 import de.decidr.ui.controller.UIDirector;
 
 /**
  * TODO: add comment
- *
+ * 
  * @author AT
  */
-public class ModelingTool extends AbstractComponent {
+public class ModelingTool extends AbstractComponent implements DataExchanger {
+
+    private HttpSession session = null;
+
+    private Long userId = null;
+
+    private Long tenantId = null;
+
+    private String tenantName = null;
+
+    private TenantFacade tenantFacade = null;
+
+    private HashMap<Long, String> userList = null;
     
-    HttpSession session = null;
-    Long id = null;
-    
-    private UIDirector uiDirector = UIDirector.getInstance();
-    private SiteFrame siteFrame = uiDirector.getTemplateView();
-      
-    private WorkflowModelsComponent component = null;
-    private CurrentTenantModelTable table = null;
-    
+    private Server server1 = null;
+
     @Override
     public String getTag() {
         return "modelingtool";
     }
-    
-    /* (non-Javadoc)
-     * @see com.vaadin.ui.AbstractComponent#paintContent(com.vaadin.terminal.PaintTarget)
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.vaadin.ui.AbstractComponent#paintContent(com.vaadin.terminal.PaintTarget
+     * )
      */
     @Override
     public void paintContent(PaintTarget target) throws PaintException {
-        // TODO Auto-generated method stub
+        server1 = new Server();
+        Object[] server = new Object[1];
+        server[1] = server1;
+        target.addAttribute("server", server);
         super.paintContent(target);
         
-        component = (WorkflowModelsComponent)siteFrame.getContent();
-        table = component.getCurrentTenantTable();
-       
-        //Item item = table.getItem(table.getValue());
-        //Long workflowModelId = (Long)item.getItemProperty("id").getValue();
-        
-        target.addAttribute("workflowModelId", 10L);
-        
-        session = Main.getCurrent().getSession();
-        
-        id = (Long)session.getAttribute("userId");
-        
-        DWDLIOServiceAsyncImpl.setUserId(id);
-        
-        //target.addAttribute("userId", id);
         
     }
-    
-    /* (non-Javadoc)
-     * @see com.vaadin.ui.AbstractComponent#changeVariables(java.lang.Object, java.util.Map)
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.vaadin.ui.AbstractComponent#changeVariables(java.lang.Object,
+     * java.util.Map)
      */
     @Override
     public void changeVariables(Object source, Map variables) {
         // TODO Auto-generated method stub
         super.changeVariables(source, variables);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.decidr.modelingtool.client.io.DataExchanger#getUsers()
+     */
+    @Override
+    public HashMap<Long, String> getUsers() {
+        session = Main.getCurrent().getSession();
+        userId = (Long) session.getAttribute("userId");
+        tenantName = (String) session.getAttribute("tenant");
+        tenantFacade = new TenantFacade(new UserRole(userId));
+        try {
+            tenantId = tenantFacade.getTenantId(tenantName);
+            List<Item> users = tenantFacade.getUsersOfTenant(tenantId, null);
+            for (Item item : users) {
+                if (!item.getItemProperty("username").equals("")) {
+                    userList.put((Long) item.getItemProperty("id").getValue(),
+                            (String) item.getItemProperty("first_name")
+                                    .getValue()
+                                    +" " + (String) item
+                                            .getItemProperty("last_name")
+                                            .getValue()
+                                    + " ("
+                                    + (String) item.getItemProperty("username")
+                                            .getValue() + ")");
+                } else {
+                    userList.put((Long) item.getItemProperty("id").getValue(),
+                            (String) item.getItemProperty("email").getValue());
+                }
+            }
+            return userList;
+        } catch (TransactionException exception) {
+            Main.getCurrent().addWindow(new TransactionErrorDialogComponent());
+            return null;
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.decidr.modelingtool.client.io.DataExchanger#loadDWDL()
+     */
+    @Override
+    public String loadDWDL() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * de.decidr.modelingtool.client.io.DataExchanger#saveDWDL(java.lang.String)
+     */
+    @Override
+    public void saveDWDL(String dwdl) {
+        // TODO Auto-generated method stub
+
     }
 
 }
