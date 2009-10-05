2f3a8d593d8cf0915db4c9cd81765de5b0136185
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-10-05 16:51:49 +0000

    RR: prevent continuous addition of new appenders

--- src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
+++ src/model/trunk/src/main/java/de/decidr/model/logging/DefaultLogger.java
@@ -1,7 +1,8 @@
 package de.decidr.model.logging;
 
+import java.util.Enumeration;
+
 import org.apache.log4j.Appender;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -23,11 +24,29 @@ public class DefaultLogger {
 
     static {
         // run configurator - every new logger will inherit the defaultAppender
-        BasicConfigurator.configure(defaultAppender);
-        // set default logging options
-        Logger.getLogger("de.decidr").setLevel(Level.DEBUG);
-        // Don't pass messages to higher-level loggers
-        Logger.getLogger("de.decidr").setAdditivity(false);
+        // RR remove if unnecessary
+        // BasicConfigurator.resetConfiguration();
+        // BasicConfigurator.configure(defaultAppender);
+
+        boolean configured = false;
+        Appender current;
+        Enumeration<Appender> appenders = Logger.getLogger("de.decidr")
+                .getAllAppenders();
+        while (appenders.hasMoreElements()) {
+            current = appenders.nextElement();
+            if (current.equals(defaultAppender)
+                    || current instanceof ConsoleAppender) {
+                configured = true;
+            }
+        }
+
+        if (!configured) {
+            Logger.getLogger("de.decidr").addAppender(defaultAppender);
+            // set default logging options
+            Logger.getLogger("de.decidr").setLevel(Level.DEBUG);
+            // Don't pass messages to higher-level loggers
+            Logger.getLogger("de.decidr").setAdditivity(false);
+        }
     }
 
     public static Logger getLogger(Class<?> clazz) {
