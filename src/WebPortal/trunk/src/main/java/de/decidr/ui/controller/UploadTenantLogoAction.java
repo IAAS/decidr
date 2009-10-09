b43a171bd74d157d2ecb363e1ed68d97b65608dc
aleks.tolev <aleks.tolev@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-10-09 09:59:03 +0000

    AT: Modified upload tenant logo action. Make upload possible

--- src/WebPortal/trunk/src/main/java/de/decidr/ui/controller/UploadTenantLogoAction.java
+++ src/WebPortal/trunk/src/main/java/de/decidr/ui/controller/UploadTenantLogoAction.java
@@ -31,30 +31,35 @@ import com.vaadin.ui.Upload.SucceededEvent;
 
 import de.decidr.model.acl.roles.UserRole;
 import de.decidr.model.exceptions.TransactionException;
+import de.decidr.model.facades.FileFacade;
 import de.decidr.model.facades.TenantFacade;
 import de.decidr.ui.view.Main;
 import de.decidr.ui.view.TransactionErrorDialogComponent;
 
 /**
  * This action handles the upload of the tenant logo
- *
+ * 
  * @author Geoff
  */
-public class UploadTenantLogoAction implements Upload.SucceededListener, 
-                                               Upload.FailedListener, 
-                                               Upload.Receiver{
-	
+public class UploadTenantLogoAction implements Upload.SucceededListener,
+        Upload.FailedListener, Upload.Receiver {
+
     private HttpSession session = Main.getCurrent().getSession();
 
-    private Long userId = (Long)session.getAttribute("userId");
-    private TenantFacade tenantFacade = new TenantFacade(new UserRole(userId)); 
-    
+    private Long userId = (Long) session.getAttribute("userId");
+    private TenantFacade tenantFacade = new TenantFacade(new UserRole(userId));
+    private FileFacade fileFacade = new FileFacade(new UserRole(userId));
+
     private File file = null;
     private Item tenant = null;
-    	
-    /* (non-Javadoc)
-    * @see com.vaadin.ui.Upload.SucceededListener#uploadSucceeded(com.vaadin.ui.Upload.SucceededEvent)
-    */
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.vaadin.ui.Upload.SucceededListener#uploadSucceeded(com.vaadin.ui.
+     * Upload.SucceededEvent)
+     */
     @Override
     public void uploadSucceeded(SucceededEvent event) {
         FileInputStream fis = null;
@@ -62,46 +67,58 @@ public class UploadTenantLogoAction implements Upload.SucceededListener,
             fis = new FileInputStream(file);
             int index = file.getCanonicalPath().indexOf('.');
             String suffix = file.getCanonicalPath().substring(index);
-            
-            tenant = (Item)session.getAttribute("tenant");
-            tenantFacade.setLogo((Long)tenant.getItemProperty("id").getValue(), fis, suffix, file.getName());
-            
+
+            tenant = (Item) session.getAttribute("tenant");
+            Long fileId = fileFacade.createFile(fis, event.getFilename(), event
+                    .getMIMEType(), true);
+            de.decidr.model.entities.File file = fileFacade.getFileInfo(fileId);
+            tenantFacade.setLogo(
+                    (Long) tenant.getItemProperty("id").getValue(), file
+                            .getId());
+
         } catch (final java.io.FileNotFoundException e) {
             Main.getCurrent().addWindow(new TransactionErrorDialogComponent());
-        }catch(TransactionException exception){
+        } catch (TransactionException exception) {
             Main.getCurrent().addWindow(new TransactionErrorDialogComponent());
-        }catch(IOException exception){
+        } catch (IOException exception) {
             Main.getCurrent().addWindow(new TransactionErrorDialogComponent());
         }
     }
-    
-    /* (non-Javadoc)
-    * @see com.vaadin.ui.Upload.FailedListener#uploadFailed(com.vaadin.ui.Upload.FailedEvent)
-    */
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.vaadin.ui.Upload.FailedListener#uploadFailed(com.vaadin.ui.Upload
+     * .FailedEvent)
+     */
     @Override
     public void uploadFailed(FailedEvent event) {
-        Main.getCurrent().getMainWindow().addWindow(new TransactionErrorDialogComponent());
+        Main.getCurrent().getMainWindow().addWindow(
+                new TransactionErrorDialogComponent());
     }
-    
-    /* (non-Javadoc)
-    * @see com.vaadin.ui.Upload.Receiver#receiveUpload(java.lang.String, java.lang.String)
-    */
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.vaadin.ui.Upload.Receiver#receiveUpload(java.lang.String,
+     * java.lang.String)
+     */
     @Override
     public OutputStream receiveUpload(String filename, String MIMEType) {
         FileOutputStream fos = null;
-        
-        
+
         file = new File(filename);
-        
+
         try {
             fos = new FileOutputStream(file);
         } catch (final java.io.FileNotFoundException e) {
             Main.getCurrent().addWindow(new TransactionErrorDialogComponent());
             return null;
         }
-        
-        return fos; 
-    
+
+        return fos;
+
     }
 
 }
