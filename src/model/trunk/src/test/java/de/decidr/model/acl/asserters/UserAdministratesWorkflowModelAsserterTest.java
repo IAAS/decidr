63c3fcdd4c8f1f12230a46457259a4a957416d69
geoffrex <geoffrex@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-10-11 13:45:33 +0000

    GH: added stubs for ACL tests

---
+++ src/model/trunk/src/test/java/de/decidr/model/acl/asserters/UserAdministratesWorkflowModelAsserterTest.java
@@ -0,0 +1,46 @@
+/*
+ * The DecidR Development Team licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package de.decidr.model.acl.asserters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * TODO: add comment
+ *
+ * @author GH
+ */
+public class UserAdministratesWorkflowModelAsserterTest {
+
+    /**
+     * Test method for {@link de.decidr.model.acl.asserters.UserAdministratesWorkflowModelAsserter#transactionStarted(de.decidr.model.transactions.TransactionEvent)}.
+     */
+    @Test
+    public void testTransactionStarted() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link de.decidr.model.acl.asserters.UserAdministratesWorkflowModelAsserter#assertRule(de.decidr.model.acl.roles.Role, de.decidr.model.acl.permissions.Permission)}.
+     */
+    @Test
+    public void testAssertRule() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/model/trunk/src/test/java/de/decidr/model/acl/roles/SuperAdminRoleTest.java
@@ -0,0 +1,19 @@
+package de.decidr.model.acl.roles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SuperAdminRoleTest {
+
+    @Test
+    public void testSuperAdminRoleLong() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSuperAdminRole() {
+        fail("Not yet implemented");
+    }
+
+}
