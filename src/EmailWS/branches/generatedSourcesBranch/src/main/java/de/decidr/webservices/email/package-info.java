6a89d7f69763fea358d29184db703d6132fbe602
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-10-29 16:31:54 +0000

    RR: removing unused branch

--- src/EmailWS/branches/generatedSourcesBranch/src/main/java/de/decidr/webservices/email/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://decidr.de/schema/DecidrTypes", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.decidr.webservices.email;
-
