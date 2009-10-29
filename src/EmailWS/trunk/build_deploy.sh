448f6b35aa7f0768eec0aacc843be2df59971d5b
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-10-29 11:37:04 +0000

    RR: some temporary adjustments to try and make Axis cooperate

--- src/EmailWS/trunk/build_deploy.sh
+++ src/EmailWS/trunk/build_deploy.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "$(readlink -e "${0}")")"
 
-(cd ../model && mvn clean install -Dmysql_client=mysql -Dmysql_root=root -Dmysql_rtpwd=rtpwd -Dmaven.test.skip=true ) && \
+(cd ../model && mvn clean install -Dmysql_host=localhost -Dmysql_root=root -Dmysql_rtpwd=rtpwd -Dmaven.test.skip=true ) && \
 mvn clean package && \
 ssh -x root@ec2-174-129-25-57.compute-1.amazonaws.com /etc/init.d/tomcat stop && sleep 3 && \
-scp target/Email-0.0.*-*.jar root@ec2-174-129-25-57.compute-1.amazonaws.com:/opt/tomcat5.5/webapps/axis2/WEB-INF/servicejars/ && \
+scp target/Email-0.0.*-*.jar root@ec2-174-129-25-57.compute-1.amazonaws.com:/opt/tomcat5.5/webapps/axis2_15/WEB-INF/servicejars/ && \
 ssh -x root@ec2-174-129-25-57.compute-1.amazonaws.com /etc/init.d/tomcat start
