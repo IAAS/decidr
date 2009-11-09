7780976cc2f96dc5be4ccecee4f5ea752c337666
rumbergerr <rumbergerr@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-11-09 20:49:01 +0000

    RR: making sure all files contain the disclaimer

--- src/ODEMonitorClient/trunk/scripts/mac/getCPUUsage.sh
+++ src/ODEMonitorClient/trunk/scripts/mac/getCPUUsage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # The DecidR Development Team licenses this file to you under
 # the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License.
