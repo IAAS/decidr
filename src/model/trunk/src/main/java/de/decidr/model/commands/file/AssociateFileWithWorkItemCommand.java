8d60d36a1608ab028711158fbbaf9dfe89a89e4e
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-12-12 12:38:52 +0000

    DH: fixed hql query typo.

--- src/model/trunk/src/main/java/de/decidr/model/commands/file/AssociateFileWithWorkItemCommand.java
+++ src/model/trunk/src/main/java/de/decidr/model/commands/file/AssociateFileWithWorkItemCommand.java
@@ -66,7 +66,7 @@ public class AssociateFileWithWorkItemCommand extends FileCommand {
             throws TransactionException {
 
         // persist file
-        String hql = "update file set temporary = false where id = :fileId";
+        String hql = "update File set temporary = false where id = :fileId";
         if (evt.getSession().createQuery(hql).setLong("fileId", getFileId())
                 .executeUpdate() == 0) {
             throw new EntityNotFoundException(File.class, getFileId());
