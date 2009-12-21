ee6ce62a8cabf93a9e17fde72e3392cd2b618488
hussdl <hussdl@53786140-b4cc-11dd-858c-a3a1ac847323>
2009-12-21 17:22:26 +0000

    DH: possible fix for StartableWorkflowModelFilter

--- src/model/trunk/src/main/java/de/decidr/model/filters/StartableWorkflowModelFilter.java
+++ src/model/trunk/src/main/java/de/decidr/model/filters/StartableWorkflowModelFilter.java
@@ -1,12 +1,17 @@
 package de.decidr.model.filters;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.type.Type;
+
+import de.decidr.model.entities.StartableWorkflowModelView;
 
 /**
  * Allows only startable models. A workflow model is considered startable if its
- * executable flag is set to true and a deployed workflow model that has the
- * same version exists.
+ * executable flag is set to true (which implies that deployed workflow model
+ * which has the same version exists).
  * 
  * @author Daniel Huss
  * @version 0.1
@@ -17,8 +22,10 @@ public class StartableWorkflowModelFilter implements Filter {
      * {@inheritDoc}
      */
     public void apply(Criteria criteria) {
-        criteria.add(Restrictions.eq("executable", true));
-        criteria.createCriteria("deployedWorkflowModels", "dwm").add(
-                Restrictions.eqProperty("dwm.version", "version"));
+        DetachedCriteria exists = DetachedCriteria
+                .forClass(StartableWorkflowModelView.class);
+        exists.add(Restrictions.eqProperty("id", criteria.getAlias() + ".id"));
+        exists.setProjection(Projections.sqlProjection("*", new String[0],
+                new Type[0]));
     }
 }
